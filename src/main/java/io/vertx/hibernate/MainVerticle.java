167fbc3f5f93dc418057d6346853010ffc1d5331
Davide <davide@hibernate.org>
2021-10-20 12:43:13 +0100

    Refactoring and fix problems

--- src/main/java/io/vertx/hibernate/MainVerticle.java
+++ src/main/java/io/vertx/hibernate/MainVerticle.java
@@ -1,51 +1,73 @@
 package io.vertx.hibernate;
 
-import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.Single;
-import io.vertx.rxjava3.core.AbstractVerticle;
-import lombok.extern.log4j.Log4j2;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import javax.persistence.Persistence;
+
 import org.hibernate.reactive.stage.Stage;
 
-import javax.persistence.Persistence;
-import java.util.List;
+import io.reactivex.rxjava3.core.Completable;
+import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.core.Promise;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class MainVerticle extends AbstractVerticle {
-  private Completable prepareData(final Stage.SessionFactory factory) {
-    return Single.fromCompletionStage(factory.withSession(
-        session -> {
-          var bossEntity = Employee.builder().name("boss").tags(List.of("tag2", "tag4")).build();
-          var employeeEntity =
-            Employee.builder()
-              .name("employee")
-              .boss(bossEntity)
-              .tags(List.of("tag1", "tag2"))
-              .build();
-          var employee2Entity =
-            Employee.builder()
-              .name("employee 2")
-              .boss(employeeEntity)
-              .tags(List.of("tag3", "tag4"))
-              .build();
-          bossEntity.boss(employee2Entity);
-          return session
-            .persist(employeeEntity, employee2Entity, bossEntity)
-            .thenApply((v) -> List.of(bossEntity, employeeEntity, employee2Entity))
-            ;
-        }))
-      .doOnSuccess(
-        (e) -> {
-          log.info("Employee: {} - {}", e.get(1).id(), e.get(1).name());
-          log.info("Employee2: {} - {}", e.get(2).id(), e.get(2).name());
-          log.info("Boss: {} - {}", e.get(0).id(), e.get(0).name());
-        })
-      .ignoreElement();
+
+  Stage.SessionFactory factory;
+
+  private Completable prepareData(Stage.SessionFactory factory) {
+    return Completable.fromCompletionStage( persist( factory ) );
+  }
+
+  private CompletionStage<Void> persist(Stage.SessionFactory factory) {
+    System.out.println( "Preparing data" );
+    var bossEntity = Employee.builder().name( "boss" ).tags( List.of( "tag2", "tag4" ) ).build();
+    var employeeEntity =
+      Employee.builder()
+        .name( "employee" )
+        .boss( bossEntity )
+        .tags( List.of( "tag1", "tag2" ) )
+        .build();
+    var employee2Entity =
+      Employee.builder()
+        .name( "employee 2" )
+        .boss( employeeEntity )
+        .tags( List.of( "tag3", "tag4" ) )
+        .build();
+    bossEntity.boss( employee2Entity );
+    return factory.withTransaction( (session, t) -> session.persist( employeeEntity, employee2Entity, bossEntity ) )
+      .thenApply( v -> List.of( bossEntity, employeeEntity, employee2Entity ) )
+      .thenAccept( e -> {
+        log.info( "Employee: {} - {}", e.get( 1 ).id(), e.get( 1 ).name() );
+        log.info( "Employee2: {} - {}", e.get( 2 ).id(), e.get( 2 ).name() );
+        log.info( "Boss: {} - {}", e.get( 0 ).id(), e.get( 0 ).name() );
+      } );
+  }
+
+  private void startHibernate(Promise<Stage.SessionFactory> promise) {
+    try {
+      this.factory = Persistence
+        .createEntityManagerFactory( "postgresql" )
+        .unwrap( Stage.SessionFactory.class );
+      promise.complete( factory );
+    }
+    catch (Exception e) {
+      promise.fail( e );
+    }
   }
 
   @Override
   public Completable rxStart() {
-    return Single.fromCallable(() -> Persistence.createEntityManagerFactory("postgresql")
-        .unwrap(Stage.SessionFactory.class))
-      .flatMapCompletable(this::prepareData);
+    System.out.println( "Starting" );
+    return vertx
+      .executeBlocking( this::startHibernate )
+      .concatMapCompletable( this::prepareData );
+  }
+
+  @Override
+  public Completable rxStop() {
+    factory.close();
+    return Completable.complete();
   }
 }
--- src/test/java/com/ukonnra/wonderland/whiterabbit/TestMainVerticle.java
+++ src/test/java/com/ukonnra/wonderland/whiterabbit/TestMainVerticle.java
@@ -1,22 +1,26 @@
 package com.ukonnra.wonderland.whiterabbit;
 
-import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.hibernate.MainVerticle;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    vertx.deployVerticle( new MainVerticle() )
+      .onSuccess( event -> testContext.completeNow() )
+      .onFailure( testContext::failNow );
   }
 
   @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+  void verticle_deployed(Vertx vertx, VertxTestContext testContext) {
     testContext.completeNow();
   }
 }
